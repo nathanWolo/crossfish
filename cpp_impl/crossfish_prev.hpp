1b9961a4658937f40207ac2a7acc8054784fb950
Nathan Woloshyn <nathwolo@amazon.com>
2026-09-06 18:51:17 +0000

    Freeze aspiration-reset SPRT winner
    20 ms SPRT: N=5952 W=2298 D=1566 L=2088, +12.26 +/- 7.58 Elo, LLR +3.09.

--- cpp_impl/crossfish_prev.hpp
+++ cpp_impl/crossfish_prev.hpp
@@ -596,6 +596,7 @@ class CrossfishPrev {
                     beta += aspiration_window;
                 }
                 else {
+                    aspiration_window = ASP_PAWNS * eval_weights[PAWN_IDX];
                     alpha = eval - aspiration_window;
                     beta = eval + aspiration_window;
                     depth++;
